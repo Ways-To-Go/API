d51143a01d284737a4fd1da57a3f85eacdf8c41e
Aymerik DIEBOLD <diebold.aymerik@gmail.com>
2020-05-02 00:09:14 +0200

    Change github sync

--- public/github-sync.php
+++ public/github-sync.php
@@ -1,2 +1,2 @@
 <?php
-`cd .. && git pull && rm -r var/cache 2>&1 result-command.txt`;
+`cd .. && git pull && php bin/console doctrine:migrations:migrate --no-interaction && rm -r var/cache 2>&1 result-command.txt`;
