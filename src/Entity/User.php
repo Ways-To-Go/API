2521b8d233377741719b474e5f7dd2f7138af427
Aymerik DIEBOLD <diebold.aymerik@gmail.com>
2020-06-07 17:24:30 +0200

    Add import

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Symfony\Component\Serializer\Annotation\SerializedName;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
